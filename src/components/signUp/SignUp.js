f351632e314b28e2b78d80a2631b9543b0d5e094
Jenna Link <jenna.hartley@pushinteractions.com>
2021-03-31 18:58:07 -0600

    Logging in after sign up, but not redirecting

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import useControlledInput from '../hooks/useControlledInput'
 import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
@@ -7,10 +8,11 @@ import Surround from '../common/Surround'
 import Visibility from '../common/Visibility'
 import './SignUp.css'
 
-import { register } from '../../reducers/loginReducer'
+import { register, login } from '../../reducers/loginReducer'
 
 const SignUp = () => {
     const dispatch = useDispatch()
+    const history = useHistory()
 
     const email = useControlledInput('text')
     const password = useControlledInput('password')
@@ -18,26 +20,48 @@ const SignUp = () => {
     const displayName = useControlledInput('text)')
     const [error, setError] = useState(null)
 
+    //todo loading states on this and login
     const onSignUpSubmit = async (event) => {
         event.preventDefault()
 
-        //todo make sure passwords are the same
+        const enteredEmail = email.value
+        const enteredPassword = password.value
+
+        if (password.value !== reEnterPassword.value) {
+            setError('Passwords do not match')
+            return
+        }
+
         const result = await dispatch(register({
             password: password.value,
             email: email.value,
             displayName: displayName.value
         }))
 
-
         switch (result.type) {
         case register.fulfilled.toString():
-            //todo login
-            console.log('register success')
+            await onSignUpComplete(enteredEmail, enteredPassword)
             break
         case register.rejected.toString():
             setError(result.payload)
+            break
         }
+    }
 
+    const onSignUpComplete = async (enteredEmail, enteredPassword) => {
+        const result = dispatch(login({
+            email: enteredEmail,
+            password: enteredPassword
+        }))
+
+        switch (result.type) {
+        case login.fulfilled:
+            history.push('/') //todo redirect to where you were?
+            break
+        case login.rejected:
+            setError(result.payload)
+            break
+        }
     }
 
     return (
