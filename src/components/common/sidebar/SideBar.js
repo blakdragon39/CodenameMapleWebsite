f2f4618b3ea43cf1687446cb5a13011a1bf7706d
Jenna Link <jenna.hartley@pushinteractions.com>
2021-04-04 21:30:34 -0600

    Added endpoint to get pets

--- src/components/common/sidebar/SideBar.js
+++ src/components/common/sidebar/SideBar.js
@@ -6,7 +6,7 @@ import './SideBar.css'
 const SideBar = () => {
     return (
         <div className='sideBar'>
-            <CurrentPet pet={'Arcanine'}/>
+            <CurrentPet />
         </div>
     )
 }
---
+++ src/services/pets.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+
+const baseUrl = `${process.env.REACT_APP_rootHost}/api/pets`
+
