957f7e96db5b48b1b4e5cf1e0f1ced532ca44f64
Jenna Link <jenna.hartley@pushinteractions.com>
2021-04-27 18:06:46 -0600

    Can click on items to bring up a Use option

--- src/components/items/MyItems.js
+++ src/components/items/MyItems.js
@@ -1,24 +1,60 @@
-import React  from 'react'
-
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import usePendingState from '../hooks/usePendingState'
 import userItemsService from '../../services/userItems'
 import { useUser } from '../hooks/userHooks'
+import Button from 'react-bootstrap/Button'
 
 import Error from '../common/Error'
 import Pending from '../common/Pending'
-import ItemList from './ItemList'
+import Visibility from '../common/Visibility'
+
+import './MyItems.css'
 
 const MyItems = () => {
     const user = useUser()
     const itemState = usePendingState([], () => userItemsService.getItems(user.token, user.id))
+    const [selectedItemIndex, setSelectedItemIndex] = useState()
 
     return (
         <div>
             <Error error={itemState.error} />
             <Pending pending={itemState.pending} />
-            <ItemList items={itemState.state} />
+            <div>
+                {
+                    itemState.state.map((item, index) =>
+                        <Item
+                            item={item}
+                            index={index}
+                            selectedItemIndex={selectedItemIndex}
+                            setSelectedItemIndex={setSelectedItemIndex}
+                            key={index} />)
+                }
+            </div>
         </div>
     )
 }
+
+const Item = ({ item, index, selectedItemIndex, setSelectedItemIndex }) => {
+    const className = index === selectedItemIndex ? 'myItem selectedItem' : 'myItem'
+
+    return (
+        <div className={className} onClick={() => setSelectedItemIndex(index)}>
+            <div><b>{ item.displayName }</b></div>
+            <div className='myItemDescription'>
+                - { item.description }
+            </div>
+            <Visibility isVisible={index === selectedItemIndex}><Button>Use this item!</Button></Visibility>
+        </div>
+    )
+}
+
+Item.propTypes = {
+    item: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    selectedItemIndex: PropTypes.number.isRequired,
+    setSelectedItemIndex: PropTypes.func.isRequired
+}
+
 
 export default MyItems
